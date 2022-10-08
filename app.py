e2cdb693f1bd5e32a942bc95b30a857885354ca0
RohitR311 <rohit.rajan031101@gmail.com>
2022-10-08 19:17:45 +0530

    flask commit

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def gen(camera):
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
-@app.route('/', methods=["GET", "POST"])
+@app.route('/', methods=["POST"])
 def index_view():
     global ear_thresh, wait_time
 
